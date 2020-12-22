8e9847fb9b3b5299c9f00794d2baf42d40ce0eec
Edtlsoft <edwardlopez2824@gmail.com>
2020-12-22 16:09:11 -0500

    [add] Migrations and models of employees, roles and permiissions

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,4 +40,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function employee()
+    {
+        return $this->belongsTo(Employee::class);
+    }
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class);
+    }
 }
